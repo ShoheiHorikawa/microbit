575e37d74f00afdfd40237958440d2a62020c32c
ShoheiHorikawa <acmilan.sheva07@gmail.com>
2023-06-28 17:54:02 +0900

    modify file

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import serial
 import time
 from slack_sdk import WebClient
 
-ser = serial.Serial("/dev/tty.usbmodem142102", 115200)
+ser = serial.Serial("COM3", 115200)
 message = [
     "今はウェルカムですよ。",
     "集中しているので話しかけないでください。",
@@ -16,8 +16,8 @@ current_condition = 100
 start_time = time.time()
 last_print_time = start_time
 
-with open("token.txt", mode="r", encoding="utf-8") as f:
-    SLACK_ACCESS_TOKEN = f.read()
+# with open("token.txt", mode="r", encoding="utf-8") as f:
+#     SLACK_ACCESS_TOKEN = f.read()
 
 
 def set_slack_status(token, status_text, status_emoji):
@@ -55,7 +55,7 @@ def main():
             current_condition = condition
             status_text = message[current_condition]
             status_emoji = emoji[current_condition]
-            set_slack_status(SLACK_ACCESS_TOKEN, status_text, status_emoji)
+            # set_slack_status(SLACK_ACCESS_TOKEN, status_text, status_emoji)
             end_time = time.time()
             run_time = end_time - start_time
             time_data[current_condition] += run_time
